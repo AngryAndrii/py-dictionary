5b43e700ac582665414d82df7b05371cdb49f1d1
Andrii Chychkan <mywork1728@gmail>
2025-11-17 15:23:43 +0200

    finished basic task

--- app/main.py
+++ app/main.py
@@ -1,15 +1,20 @@
-import pprint
+from typing import Any, Union
+
 
 class Dictionary:
 
-    def __init__(self, capacity = 8):
+    def __init__(self, capacity: int = 8) -> None:
         self.table = [None] * capacity
         self.length = 0
         self.capacity = capacity
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: Any, value: Any) -> None:
+
         hash_value = hash(key)
+        node = {"key": key, "value": value, "hash": hash_value}
+
         self.check_need_resize()
+
         index = self.count_position(hash_value)
 
         if self.table[index] is not None:
@@ -21,35 +26,29 @@ class Dictionary:
                     break
             if not found:
                 self.length += 1
-                self.table[index].append({"key": key,
-                                      "value": value,
-                                      "hash": hash_value})
+                self.table[index].append(node)
 
         else:
             self.length += 1
             self.table[index] = list()
-            self.table[index].append({"key": key,
-                                      "value": value,
-                                      "hash": hash_value})
+            self.table[index].append(node)
 
-
-    def __getitem__(self, key):
+    def __getitem__(self, key: Any) -> Any:
         search_hash = hash(key)
         index = self.count_position(search_hash)
         if self.table[index] is not None:
             for el in self.table[index]:
                 if el["hash"] == search_hash and el["key"] == key:
                     return el["value"]
-        raise KeyError("Key not found")
+        raise KeyError(f"Key {key} not found!")
 
-    def check_need_resize(self):
-        load_factor = 2/3
+    def check_need_resize(self) -> None:
+        load_factor = 2 / 3
 
         if (self.length + 1) / self.capacity >= load_factor:
             self._resize()
 
-
-    def _resize(self):
+    def _resize(self) -> None:
         new_capacity = self.capacity * 2
         new_table = [None] * new_capacity
 
@@ -69,53 +68,11 @@ class Dictionary:
         self.table = new_table
         self.capacity = new_capacity
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.length
 
-    def count_position(self, hash_value: int, capacity = None):
+    def count_position(self, hash_value: int,
+                       capacity: Union[int, None] = None) -> Union[float, int]:
         if capacity is None:
             capacity = self.capacity
         return hash_value % capacity
-
-
-
-
-
-
-custom_dict = Dictionary()
-
-custom_dict.__setitem__("forelka", 124)
-custom_dict.__setitem__("jokdf", 845)
-custom_dict.__setitem__("for34lka", 194)
-# custom_dict.__setitem__("jo23ff", 8695)
-custom_dict.__setitem__("jilkkjla", 124)
-custom_dict.__setitem__("orgrgrgo", 8444)
-custom_dict.__setitem__("fllllllllll", 19633)
-custom_dict.__setitem__("uoibrrrr", 5)
-custom_dict.__setitem__("bobre", 88)
-custom_dict.__setitem__("lupka", 14)
-custom_dict.__setitem__("кеа", 124)
-custom_dict.__setitem__("йц", 8444)
-# custom_dict.__setitem__("412431", 19633)
-# custom_dict.__setitem__("sdfa", 124)
-# custom_dict.__setitem__("jjy", 8444)
-# custom_dict.__setitem__("412431", 19633)
-# custom_dict.__setitem__("bobre", 84)
-#
-pprint.pprint(custom_dict.table)
-
-
-# inside of node - tuple, dict, or little class?
-
-# node - key, hash, value
-
-# i should save hash in node
-
-# if i do set - search node in cell with same key, then equal hash and equal
-# key then add item and increase length
-# then - check load-factor
-
-# get - search cell - hash and then key if no -- ValueError
-#
-
-# cell = [{key: 1, value: 2, hash: 123123}, {key: 2, value: 55, hash: 098776}]
